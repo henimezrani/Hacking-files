62fd45daf6768a0c3d51f238ddd36c14807792e2
henimezrani <56113227+henimezrani@users.noreply.github.com>
2019-10-26 17:03:13 +0100

    Add files via upload

---
+++ 3-5-1 & 3-6/Html-Js-interaction/main.js
@@ -0,0 +1,73 @@
+// $('h1').css('color','blue')
+// $('body').css('background-color', 'red')
+// $('#everything').css('font-size', '1.2em')
+// $('.holder').css('border', 'solid 2px black')
+// $('ul li').css('font-weight','bold')
+// $('h1 + p').css('color','green')
+// $('#secret').css('display', 'block')
+// $("#change-me").html("it has been changed ")
+// $("#change-me").append("one more time")
+// $("#change-me").html("<p>Paragraph 1</p> <p>Paragraph 2 </p>")
+// $("#change-me").append("one more more time")
+// $("li").html("new sentance")
+// $("li").append(" hsfe")
+// $('#everything').append("<ol><li>element1</li><li>element2</li><li>element3</li></ol>")
+
+
+// var new_div = $('<div class="important">Got a new div coming in here!</div>') 
+// // can we see the new div on the page yet? 
+// new_div.css('color','red').attr('id','primary').text("Hi, I'm new here")
+// // is it on the page yet? 
+
+// $('body').append(new_div) 
+
+// // how about now? 
+
+// // what's this do? 
+// new_div.addClass('hello');
+
+
+// var array =[
+// "black",
+// "silver",
+// "gray",
+// "white",
+// "maroon",
+// "red",
+// "purple",
+// "fuchsia",
+// "green",
+// "lime",
+// "olive",
+// "yellow",
+// "navy",
+// "blue",
+// "teal",
+// "aqua",
+// "orange",
+// "aliceblue",
+// "antiquewhite",
+// "aquamarine",
+// "azure",
+// "beige",
+// "bisque",
+// "blanchedalmond",
+// "blueviolet",
+// "brown","burlywood","cadetblue","chartreuse","chocolate","coral","cornflowerblue","cornsilk","crimson","cyan","darkblue","darkcyan","darkgoldenrod","gold","grey","magenta","pink","turquoise","violet"]
+
+// var input="";
+// while (true){
+// 	input = prompt();
+// 	if ((input.length === 7 || input.length === 4) && input[0] === "#"){
+// 		break;
+// 	}
+// 	if (array.indexOf(input.toLowerCase()) > 0){
+// 		break;
+// 	} 
+// }
+
+// $('body').css("background-color",input);
+// alert("ugly " + input + " theme chosen!")
+
+
+$('h1').slideDown();
